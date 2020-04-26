68c1fe15e0c7a25f6276c4dc818075bfbe584789
Nebiyat Eskndir <63973624+Nebiyat-ccc@users.noreply.github.com>
2020-04-26 12:39:54 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,39 @@
-//start with initial text on the page 
-//see more button under text 
-// when you click see more the full text shows up 
-//see less button shows up, see more button dissappear
-// when you click see less the text shrinks back to original 
-// see more button appears, see less button dissappears 
-
-function seeMore() {
-  var paragraph = document.getElementById("font");
-  paragraph.innerHTML += "I have always want to volunteer and help people who needs help. most of the times I've seen people also myself giving food and cloth. Although this is a very great thing to do, I personally want to help people by educating. Forinstance there are girls and womens who have't got the education I got. Goode have taught me how to help others based on what we have. I believe food and cloth are the least we humans can do for others. However, education can last forever."
-
-  //make the see more button dissappear
-  var seeMoreButton = document.getElementById("see-more");
-  seeMoreButton.style.display = "none";
-
-  //see less button appears
-  var seeLessButton = document.getElementById("see-less");
-  seeLessButton.style.display = "block";
+body{
+  position:absolute;
+    width:100%;
+    height:100%;
+    left:0;
+    bottom:0;
+background:url(https://media.wired.com/photos/59266c3b7034dc5f91beb5d9/master/w_582,c_limit/BioCodingTA.jpg);
+  color:black;
+font-weight:Bold}
+h1{color:#8B008B;}
+mark{background-color:pink; 
+}
+#see-less {
+  display: none;
+}
+#see-more{
+  color:#8B0000;
 }
 
-function seeLess() {
-  var paragraph = document.getElementById("text");
-  paragraph.innerHTML = "I have always want to volunteer and help people who needs help. most of the times I've seen people also myself giving food and cloth. Although this is a very great thing to do, I personally want to help people by educating. Forinstance there are girls and womens who have't got the education I got. Goode have taught me how to help others based on what we have. I believe food and cloth are the least we humans can do for others. However, education can last forever";
-
-  var seeMoreButton = document.getElementById("see-more");
-  seeMoreButton.style.display = "block";
-
-  var seeLessButton = document.getElementById("see-less");
-  seeLessButton.style.display = "none";
+.container{
+  width:90px;
+  padding:10;
+    background:#ddd;
+    margin:
 }
+
+h1{
+  margin-right: auto;
+    margin-left:500px;
+    padding-left: 15px;
+    padding-right: 15px;
+  font-style:italic;
+  
+  padding:40px;
+}
+    
+}
+
+ 
